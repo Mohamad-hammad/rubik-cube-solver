bc4971b37176d25b5d6d78cfcd285479d7014bf9
Hammad <66684303+Mohamad-hammad@users.noreply.github.com>
2022-12-27 01:42:34 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,144 +1,364 @@
-left = [['y','o','o'],
-      ['o','y','o'],
-      ['o','o','y'],]
-right = [['r','r','b'],
-      ['r','b','r'],
-      ['b','r','r'],]
+#19L-2227: Syed M Hani
+#19L-1184: Hassan Salahuddin
+import copy
+import collections
+from collections import deque
+class Game:
+    Moves = ""
+    Face = 0
+    Back = 1
+    Top = 2
+    Bottom = 3
+    Left = 4
+    Right = 5
 
-up=[['g','w','w'],
-       ['w', 'g', 'w'],
-       ['w', 'w', 'g'],]
+    Cube = 0
+    def __init__(self):
+        self.Cube = [[['White', 'White', 'White'], ['White', 'White', 'White'], ['White', 'White', 'White']], [['Blue', 'Blue', 'Blue'], ['Blue', 'Blue', 'Blue'], ['Blue', 'Blue', 'Blue']]]
+        self.Cube.append([['Green', 'Green', 'Green'], ['Green', 'Green', 'Green'], ['Green', 'Green', 'Green']])
+        self.Cube.append([['Red', 'Red', 'Red'], ['Red', 'Red', 'Red'], ['Red', 'Red', 'Red']])
+        self.Cube.append([['Orange', 'Orange', 'Orange'], ['Orange', 'Orange', 'Orange'], ['Orange', 'Orange', 'Orange']])
+        self.Cube.append([['Yellow', 'Yellow', 'Yellow'], ['Yellow', 'Yellow', 'Yellow'], ['Yellow', 'Yellow', 'Yellow']])
+    #def __init__(self):
 
-back=[['b','b','w'],
-       ['b','w','b'],
-       ['w','b','b'],]
+    def copyGame(self, instance):
+        self = copy.deepcopy(instance)
+        return self
 
-front=[['b','g','g'],
-      ['g','b','g'],
-      ['g','g','b'],]
+    def Swap(self, a, b):
+        temp = a
+        temp2 = b
+        return temp2, temp
+#-----------------------------------Print Methods-------------------------------------
+#The self parameter is a reference to the current instance of the class, and is used to access variables that belongs to the class.
+    def PrintCube(self):
+        print('\nPrinting Cube:')
+        for i in range(3):
+            print("\t\t\t\t\t\t"+self.Cube[self.Top][i][0] +" "+self.Cube[self.Top][i][1]+" "+self.Cube[self.Top][i][2])
+        for i in range(3):
+            print(self.Cube[self.Left][i][0] + " " + self.Cube[self.Left][i][1] + " " + self.Cube[self.Left][i][2]+'\t||\t'+
+                  self.Cube[self.Face][i][0] + " " + self.Cube[self.Face][i][1] + " " + self.Cube[self.Face][i][2]+'\t||\t'+
+                  self.Cube[self.Right][i][0] + " " + self.Cube[self.Right][i][1] + " " + self.Cube[self.Right][i][2]+'\t||\t'+
+                  self.Cube[self.Back][i][0] + " " + self.Cube[self.Back][i][1] + " " + self.Cube[self.Back][i][2])
+        for i in range(3):
+            print("\t\t\t\t\t\t"+self.Cube[self.Bottom][i][0] +" "+self.Cube[self.Bottom][i][1]+" "+self.Cube[self.Bottom][i][2])
 
-down=[['y','y','r'],
-      ['y','r','y'],
-      ['r','y','y'],]
+    def PrintCube2(self):
+        print('\nPrinting Cube:')
+        for i in range(3):
+            print("\t\t\t"+self.Cube[self.Top][i][0][0] +" "+self.Cube[self.Top][i][1][0]+" "+self.Cube[self.Top][i][2][0])
+        for i in range(3):
+            print(self.Cube[self.Left][i][0][0] + " " + self.Cube[self.Left][i][1][0] + " " + self.Cube[self.Left][i][2][0]+'\t||\t'+
+                  self.Cube[self.Face][i][0][0] + " " + self.Cube[self.Face][i][1][0] + " " + self.Cube[self.Face][i][2][0]+'\t||\t'+
+                  self.Cube[self.Right][i][0][0] + " " + self.Cube[self.Right][i][1][0] + " " + self.Cube[self.Right][i][2][0]+'\t||\t'+
+                  self.Cube[self.Back][i][0][0] + " " + self.Cube[self.Back][i][1][0] + " " + self.Cube[self.Back][i][2][0])
+        for i in range(3):
+            print("\t\t\t"+self.Cube[self.Bottom][i][0][0] +" "+self.Cube[self.Bottom][i][1][0]+" "+self.Cube[self.Bottom][i][2][0])
 
-# left = [['o','o','o'],
-#       ['o','o','o'],
-#       ['o','o','o'],]
-# right = [['r','r','r'],
-#       ['r','r','r'],
-#       ['r','r','r'],]
-#
-# up=[['w','w','w'],
-#        ['w', 'w', 'w'],
-#        ['w', 'w', 'w'],]
-#
-# back=[['b','b','b'],
-#        ['b','b','b'],
-#        ['b','b','b'],]
-#
-# front=[['g','g','g'],
-#       ['g','g','g'],
-#       ['g','g','g'],]
-#
-# down=[['y','y','y'],
-#       ['y','y','y'],
-#       ['y','y','y'],]
-def frontClockwise(up,front,right,left,back):
-    temp=[0,0,0]
-    temp[0], temp[1], temp[2] = front[0][0], front[0][1], front[0][2]
-    front[0][0], front[0][1], front[0][2] = front[2][0], front[1][0], front[0][0]
-    front[1][0], front[2][0] = front[2][1], front[2][2]
-    front[2][1], front[2][2] = front[1][2], temp[2]
-    front[1][2], front[0][2]=temp[1], temp[0]
+#--------------------------Move Methods-----------------------------------------------
+    def MoveFace(self):
+        tempArr = [0,0,0]
+        for i in range(3):          #storing Right in Temp to move Top
+            tempArr[i]=self.Cube[self.Right][i][0]
+        for i in range(3):
+            self.Cube[self.Right][i][0] = self.Cube[self.Top][2][i]
+        for i in range (3):
+            tempArr[i], self.Cube[self.Bottom][0][i] = self.Swap(tempArr[i], self.Cube[self.Bottom][0][i])
+        for i in range (3):
+            tempArr[i], self.Cube[self.Left][i][2] = self.Swap(tempArr[i], self.Cube[self.Left][i][2])
+        for i in range (3):
+            tempArr[i], self.Cube[self.Top][2][i] = self.Swap(tempArr[i], self.Cube[self.Top][2][i])
+
+        # transpose of Facing side
+        temp = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        for i in range(3):
+            for j in range(3):
+                temp[i][j] = self.Cube[self.Face][i][j]
+
+        self.Cube[self.Face][0][0] = temp[2][0]
+        self.Cube[self.Face][0][1] = temp[1][0]
+        self.Cube[self.Face][0][2] = temp[0][0]
+
+        self.Cube[self.Face][1][2] = temp[0][1]
+        self.Cube[self.Face][2][2] = temp[0][2]
+
+        self.Cube[self.Face][2][1] = temp[1][2]
+        self.Cube[self.Face][2][0] = temp[2][2]
+
+        self.Cube[self.Face][1][0] = temp[2][1]
+
+    def MoveTop(self):
+        tempArr = [0,0,0]
+        for i in range(3):
+            tempArr[i]=self.Cube[self.Face][0][i]
+        for i in range(3):
+            self.Cube[self.Face][0][i] = self.Cube[self.Right][0][i]
+        for i in range (3):
+            tempArr[i], self.Cube[self.Left][0][i] = self.Swap(tempArr[i], self.Cube[self.Left][0][i])
+        for i in range (3):
+            tempArr[i], self.Cube[self.Back][0][i] = self.Swap(tempArr[i], self.Cube[self.Back][0][i])
+        for i in range (3):
+            tempArr[i], self.Cube[self.Right][0][i] = self.Swap(tempArr[i], self.Cube[self.Right][0][i])
+
+        # transpose of Facing side
+        temp = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        for i in range(3):
+            for j in range(3):
+                temp[i][j] = self.Cube[self.Top][i][j]
+
+        self.Cube[self.Top][0][0] = temp[2][0]
+        self.Cube[self.Top][0][1] = temp[1][0]
+        self.Cube[self.Top][0][2] = temp[0][0]
+
+        self.Cube[self.Top][1][2] = temp[0][1]
+        self.Cube[self.Top][2][2] = temp[0][2]
+
+        self.Cube[self.Top][2][1] = temp[1][2]
+        self.Cube[self.Top][2][0] = temp[2][2]
+
+        self.Cube[self.Top][1][0] = temp[2][1]
+
+    def MoveBack(self):
+        tempArr = [0,0,0]
+        for i in range(3):          #storing Right in Temp to move Top
+            tempArr[i]=self.Cube[self.Right][i][2]
+        for i in range(3):
+            self.Cube[self.Right][i][2] = self.Cube[self.Bottom][2][i]
+        for i in range (3):
+            tempArr[i], self.Cube[self.Top][0][i] = self.Swap(tempArr[i], self.Cube[self.Top][0][i])
+        for i in range (3):
+            tempArr[i], self.Cube[self.Left][i][0] = self.Swap(tempArr[i], self.Cube[self.Left][i][0])
+        for i in range (3):
+            tempArr[i], self.Cube[self.Bottom][2][i] = self.Swap(tempArr[i], self.Cube[self.Bottom][2][i])
+
+        # transpose of Facing side
+        temp = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        for i in range(3):
+             for j in range(3):
+                 temp[i][j] = self.Cube[self.Back][i][j]
+
+        self.Cube[self.Back][0][0] = temp[2][0]
+        self.Cube[self.Back][0][1] = temp[1][0]
+        self.Cube[self.Back][0][2] = temp[0][0]
+
+        self.Cube[self.Back][1][2] = temp[0][1]
+        self.Cube[self.Back][2][2] = temp[0][2]
+
+        self.Cube[self.Back][2][1] = temp[1][2]
+        self.Cube[self.Back][2][0] = temp[2][2]
+
+        self.Cube[self.Back][1][0] = temp[2][1]
+    def MoveBottom(self):
+        tempArr = [0,0,0]
+        for i in range(3):
+            tempArr[i]=self.Cube[self.Face][0][i]
+        for i in range(3):
+            self.Cube[self.Face][2][i] = self.Cube[self.Left][0][i]
+        for i in range (3):
+            tempArr[i], self.Cube[self.Right][2][i] = self.Swap(tempArr[i], self.Cube[self.Right][2][i])
+        for i in range (3):
+            tempArr[i], self.Cube[self.Back][2][i] = self.Swap(tempArr[i], self.Cube[self.Back][2][i])
+        for i in range (3):
+            tempArr[i], self.Cube[self.Left][2][i] = self.Swap(tempArr[i], self.Cube[self.Left][2][i])
+        # transpose of Facing side
+        temp = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        for i in range(3):
+            for j in range(3):
+                temp[i][j] = self.Cube[self.Bottom][i][j]
+
+        self.Cube[self.Bottom][0][0] = temp[2][0]
+        self.Cube[self.Bottom][0][1] = temp[1][0]
+        self.Cube[self.Bottom][0][2] = temp[0][0]
+
+        self.Cube[self.Bottom][1][2] = temp[0][1]
+        self.Cube[self.Bottom][2][2] = temp[0][2]
+
+        self.Cube[self.Bottom][2][1] = temp[1][2]
+        self.Cube[self.Bottom][2][0] = temp[2][2]
+
+        self.Cube[self.Bottom][1][0] = temp[2][1]
+
+    def MoveRight(self):
+        tempArr = [0,0,0]
+        for i in range(3):          #storing Right in Temp to move Top
+            tempArr[i]=self.Cube[self.Back][i][0]
+        for i in range(3):
+            self.Cube[self.Back][i][0] = self.Cube[self.Top][i][2]
+        for i in range (3):
+            tempArr[i], self.Cube[self.Bottom][i][2] = self.Swap(tempArr[i], self.Cube[self.Bottom][i][2])
+        for i in range (3):
+            tempArr[i], self.Cube[self.Face][i][2] = self.Swap(tempArr[i], self.Cube[self.Face][i][2])
+        for i in range (3):
+            tempArr[i], self.Cube[self.Top][i][2] = self.Swap(tempArr[i], self.Cube[self.Top][i][2])
+
+        # transpose of Facing side
+        temp = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        for i in range(3):
+            for j in range(3):
+                temp[i][j] = self.Cube[self.Right][i][j]
+
+        self.Cube[self.Right][0][0] = temp[2][0]
+        self.Cube[self.Right][0][1] = temp[1][0]
+        self.Cube[self.Right][0][2] = temp[0][0]
+
+        self.Cube[self.Right][1][2] = temp[0][1]
+        self.Cube[self.Right][2][2] = temp[0][2]
+
+        self.Cube[self.Right][2][1] = temp[1][2]
+        self.Cube[self.Right][2][0] = temp[2][2]
+
+        self.Cube[self.Right][1][0] = temp[2][1]
 
 
-    temp[0],temp[1],temp[2]=up[2][0], up[2][1], up[2][2]
-    up[2][0], up[2][1], up[2][2] = left[0][2], left[1][2], left[2][2]
-    left[0][2], left[1][2], left[2][2] = down[0][0], down[0][1], down[0][2]
-    down[0][0], down[0][1], down[0][2] = right[0][0], right[1][0], right[2][0]
-    right[0][0], right[1][0], right[2][0] = temp[0], temp[1], temp[2]
+    def MoveLeft(self):
+        tempArr = [0,0,0]
+        for i in range(3):          #storing Right in Temp to move Top
+            tempArr[i]=self.Cube[self.Back][i][2]
+        for i in range(3):
+            self.Cube[self.Back][i][2] = self.Cube[self.Bottom][i][0]
+        for i in range (3):
+            tempArr[i], self.Cube[self.Top][i][0] = self.Swap(tempArr[i], self.Cube[self.Top][i][0])
+        for i in range (3):
+            tempArr[i], self.Cube[self.Face][i][0] = self.Swap(tempArr[i], self.Cube[self.Face][i][0])
+        for i in range (3):
+            tempArr[i], self.Cube[self.Bottom][i][0] = self.Swap(tempArr[i], self.Cube[self.Bottom][i][0])
 
-def rightClockwise(up,front,right,left,back):
-    temp=[0,0,0]
-    temp[0], temp[1], temp[2] = right[0][0], right[0][1], right[0][2]
-    right[0][0], right[0][1], right[0][2] = right[2][0], right[1][0], right[0][0]
-    right[1][0], right[2][0] = right[2][1], right[2][2]
-    right[2][1], right[2][2] = right[1][2], temp[2]
-    right[1][2], right[0][2] = temp[1], temp[0]
+        # transpose of Facing side
+        temp = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        for i in range(3):
+            for j in range(3):
+                temp[i][j] = self.Cube[self.Left][i][j]
 
-    temp[0], temp[1], temp[2] = up[0][2], up[1][2], up[2][2]
-    up[0][2], up[1][2], up[2][2] =  front[0][2], front[1][2], front[2][2]
-    front[0][2], front[1][2], front[2][2] = down[0][2], down[1][2], down[2][2]
-    down[0][2], down[1][2], down[2][2] = back[2][0], back[1][0], back[0][0]
+        self.Cube[self.Left][0][0] = temp[2][0]
+        self.Cube[self.Left][0][1] = temp[1][0]
+        self.Cube[self.Left][0][2] = temp[0][0]
 
-    back[2][0], back[1][0], back[0][0] = temp[0], temp[1], temp[2]
+        self.Cube[self.Left][1][2] = temp[0][1]
+        self.Cube[self.Left][2][2] = temp[0][2]
+
+        self.Cube[self.Left][2][1] = temp[1][2]
+        self.Cube[self.Left][2][0] = temp[2][2]
+
+        self.Cube[self.Left][1][0] = temp[2][1]
+
+        #---------------------------------------Solver------------------------------
+
+def solveCube(self, goal):
+    if(self.Cube == goal.Cube):
+    #if (goal.Cube[goal.Left][0][0] != 0):
+        #print(goal.Cube[goal.Left][0][0])
+        print("Solved")
+        return goal
+    queue= deque()
+    queue.append(self)
+    flag = True
+    while (len(queue)>0)and(flag==True):
+        final_node = Game()
+
+        cur_node = queue.popleft()
+
+        g1 = Game()
+
+        g1 = copy.deepcopy(cur_node)
+
+        #g1 = g1.copyGame(cur_node)
+        g1.MoveLeft()
+        g1.Moves+=" ->  Move Left "
+        g2 = Game()
+
+        g2 = g2.copyGame(cur_node)
+
+        g2.MoveRight()
+        g2.Moves+=" -> Move Right"
+        g3 = Game()
+        g3 = copy.deepcopy(cur_node)
+
+        #g3 = g3.copyGame(cur_node)
+        g3.MoveFace()
+        g3.Moves+=" -> Move Face "
+        g4 = Game()
+        g4 = copy.deepcopy(cur_node)
+
+        #g4 = g4.copyGame(cur_node)
+        g4.MoveBottom()
+        g4.Moves+=" -> Move Bottom "
+        g5 = Game()
+        g5 = copy.deepcopy(cur_node)
+
+        #g5 = g5.copyGame(cur_node)
+        g5.MoveTop()
+        g5.Moves+=" -> Move Top "
+        g6 = Game()
+        g6 = copy.deepcopy(cur_node)
+
+        #g6 = g6.copyGame(cur_node)
+        g6.MoveBack()
+        g6.Moves+=" -> Move Back "
+
+        #print(type(cur_node))
+        print("\nWithin Solver ")
+        #cur_node.PrintCube2()
+
+        if g1.Cube != goal.Cube:
+            queue.append(g1)
+        else:#else means solution found
+            final_node = final_node.copyGame(g1)
+            flag = False
+            break
+        if g2.Cube != goal.Cube:
+            queue.append(g2)
+        else:
+            final_node = final_node.copyGame(g2)
+            flag = False
+            break
+        if g3.Cube != goal.Cube:
+            queue.append(g3)
+        else:
+            final_node =  final_node.copyGame(g3)
+            flag = False
+            break
+        if g4.Cube != goal.Cube:
+            queue.append(g4)
+        else:
+            final_node =  final_node.copyGame(g4)
+            flag = False
+            break
+        if g5.Cube != goal.Cube:
+            queue.append(g5)
+        else:
+            final_node = final_node.copyGame(g5)
+            flag = False
+            break
+        if g6.Cube != goal.Cube:
+            queue.append(g6)
+        else:
+            final_node = final_node.copyGame(g6)
+            flag = False
+            break
+
+    print("\n\nSolution:")
+    print(final_node.Moves)
+    # for i in range(len(final_node.Moves)):
+    #     print(final_node.Moves[i])
+    #print(goal.Cube[goal.Left][0][0])
+    print("Solved")
+    return final_node
+#deep copy built in function in Game class for cube
+#compare built in function in Game class for cube
 
 
-def upClockwise(up,front,right,left,back):
-    temp = [0, 0, 0]
-    temp[0], temp[1], temp[2] = up[0][0], up[0][1], up[0][2]
-    up[0][0], up[0][1], up[0][2] = up[2][0], up[1][0], up[0][0]
-    up[1][0], up[2][0] = up[2][1], up[2][2]
-    up[2][1], up[2][2] = up[1][2], temp[2]
-    up[1][2], up[0][2] = temp[1], temp[0]
+g1 = Game()
+g1.PrintCube2()
+goal = Game()
 
-    temp[0], temp[1], temp[2] = left[0][0], left[0][1], left[0][2]
-    left[0][0], left[0][1], left[0][2] = front[0][0], front[0][1], front[0][2]
-    front[0][0], front[0][1], front[0][2] = right[0][0], right[0][1], right[0][2]
-    right[0][0], right[0][1], right[0][2] = back[0][0], back[0][1], back[0][2]
-    back[0][0], back[0][1], back[0][2] = temp[0], temp[1], temp[2]
+g1.MoveLeft()
 
-def backClockwise(up,front,right,left,back):
-    temp = [0, 0, 0]
-    temp[0], temp[1], temp[2] = back[0][0], back[0][1], back[0][2]
-    back[0][0], back[0][1], back[0][2] = back[2][0], back[1][0], back[0][0]
-    back[1][0], back[2][0] = back[2][1], back[2][2]
-    back[2][1], back[2][2] = back[1][2], temp[2]
-    back[1][2], back[0][2] = temp[1], temp[0]
+g1.PrintCube2()
 
-    temp[0], temp[1], temp[2] = up[0][0], up[0][1], up[0][2]
-    up[0][0], up[0][1], up[0][2] = right[0][2], right[1][2], right[2][2]
-    right[0][2], right[1][2], right[2][2] = down[2][2], down[2][1], down[2][0]
-    down[2][2], down[2][1], down[2][0] = left[2][0], left[1][0], left[0][0]
-    left[2][0], left[1][0], left[0][0] = temp[0], temp[1], temp[2]
+"""
+g2 = Game()
+g2 = copy.deepcopy(g1)
+g2.PrintCube2()
+"""
 
-def leftClockwise(up,front,right,left,back):
-    temp = [0, 0, 0]
-    temp[0], temp[1], temp[2] = left[0][0], left[0][1], left[0][2]
-    left[0][0], left[0][1], left[0][2] = left[2][0], left[1][0], left[0][0]
-    left[1][0], left[2][0] = left[2][1], left[2][2]
-    left[2][1], left[2][2] = left[1][2], temp[2]
-    left[1][2], left[0][2] = temp[1], temp[0]
-
-    temp[0], temp[1], temp[2] = up[0][0], up[1][0], up[2][0]
-    up[0][0], up[1][0], up[2][0] = back[2][2], back[1][2], back[0][2]
-    back[2][2], back[1][2], back[0][2] = down[0][0], down[1][0], down[2][0]
-    down[0][0], down[1][0], down[2][0] = front[0][0], front[1][0], front[2][0]
-    front[0][0], front[1][0], front[2][0] = temp[0], temp[1], temp[2]
-
-def downClockwise(up,front,right,left,back):
-    temp = [0, 0, 0]
-    temp[0], temp[1], temp[2] = down[0][0], down[0][1], down[0][2]
-    down[0][0], down[0][1], down[0][2] = down[2][0], down[1][0], down[0][0]
-    down[1][0], down[2][0] = down[2][1], down[2][2]
-    down[2][1], down[2][2] = down[1][2], temp[2]
-    down[1][2], down[0][2] = temp[1], temp[0]
-
-    temp[0], temp[1], temp[2] = back[2][0], back[2][1], back[2][2]
-    back[2][0], back[2][1], back[2][2] = right[2][0], right[2][1], right[2][2]
-    right[2][0], right[2][1], right[2][2] = front[2][0], front[2][1], front[2][2]
-    front[2][0], front[2][1], front[2][2] = left[2][0], left[2][1], left[2][2]
-    left[2][0], left[2][1], left[2][2] = temp[0], temp[1], temp[2]
-def printCube(up,front,right,left,back):
-    for i in range(0,3):
-        print("          "+up[i][0]+" "+up[i][1]+" "+up[i][2]+" ")
-    print("          " + "_" + "_" +"_" + "_"+ "_" +"_")
-    for i in range(0, 3):
-        print(left[i][0]+" "+left[i][1]+" "+left[i][2]+"   | "+ front[i][0]+" "+front[i][1]+" "+front[i][2]+"  | "+ right[i][0]+" "+right[i][1]+" "+right[i][2]+"  | "+
-              back[i][0] + " " + back[i][1] + " " + back[i][2] + " ")
-    print("          " + "_" + "_" + "_" + "_" + "_" + "_")
-    for i in range(0, 3):
-        print("          " + down[i][0] + " " + down[i][1] + " " + down[i][2] + " ")
-
-
-#main driver code
+g1 = solveCube(g1,goal)
